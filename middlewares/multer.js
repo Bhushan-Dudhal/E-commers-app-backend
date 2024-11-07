1a1f61c1fd1b5ddb11ab945522d24a4ed439548a
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2024-11-07 20:58:41 +0530

    multer   file updated

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,4 +1,4 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage()
-export const singleupload = multer({ storage: storage }).single("file");
+export const singleupload = multer({ storage:storage }).single("file");
