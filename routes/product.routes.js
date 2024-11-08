2b1d6f681ef9d9fc46ec28069ffd1c8fe3e0ed9f
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2024-11-08 19:33:39 +0530

    product   file updated

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -28,6 +28,8 @@ productRoutes.delete("/delete-image/:id", isAuth,isAdmin, DeleteProductsImage)
 productRoutes.delete("/delete-product/:id", isAuth,isAdmin, DeleteProduct)
 
 productRoutes.put('/:id/review',isAuth,ProductReview)
+productRoutes.put('/:id/review',isAuth)
+
 
 
 
