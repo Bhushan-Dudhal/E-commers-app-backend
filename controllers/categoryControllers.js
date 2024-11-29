3904a89e11b8500d1ad5e329d78fdcacffcd4ad0
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2024-11-29 20:34:51 +0530

    userControllers file    updated

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -103,7 +103,7 @@ export const CategoryUpdate = async (req, res, next) => {
         }
         res.status(500).send({
             success: false,
-            message: "Error in DELETE CAT API",
+            message: "Error IN DELETE CAT API",
             error,
         });
     
